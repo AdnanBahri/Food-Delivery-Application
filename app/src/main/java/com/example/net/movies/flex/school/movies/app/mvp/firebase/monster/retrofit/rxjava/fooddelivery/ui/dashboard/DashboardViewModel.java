2fdb6c06e09095a1fcdc8850f7d582d54067b33f
Adnan <adnanbahri4@gmail.com>
2022-05-16 21:29:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/net/movies/flex/school/movies/app/mvp/firebase/monster/retrofit/rxjava/fooddelivery/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package com.example.net.movies.flex.school.movies.app.mvp.firebase.monster.retrofit.rxjava.fooddelivery.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
